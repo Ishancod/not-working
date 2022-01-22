8df99ad0f8c0145d5700d94dc767725c90b32c1e
Ishancod <88098038+Ishancod@users.noreply.github.com>
2022-01-22 19:55:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var bird, slingShot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,17 +38,15 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    constrainedLog = new Log(230,180,80,PI/2)
-    chain= new Chain(bird.body,constrainedLog.body)
-
+    //log6 = new Log(230,180,80, PI/2);
+    slingShot = new Slingshot(bird.body,{x:200,y:100});
 }
 
 function draw(){
+
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -66,6 +64,16 @@ function draw(){
 
     bird.display();
     platform.display();
-    chain.display();
-    constrainedLog.display();
+    //log6.display();
+    slingShot.display();
+
 }
+ function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+
+ }
+ function mouseReleased(){
+slingShot.fly()
+
+ }
+
